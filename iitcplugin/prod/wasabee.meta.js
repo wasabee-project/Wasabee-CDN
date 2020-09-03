7299e523fa813c858bada9c138de1731560f9a56
Scot Bontrager <scot@indievisible.org>
2020-09-03 11:18:02 -0500

    bugfixes

--- iitcplugin/prod/wasabee.meta.js
+++ iitcplugin/prod/wasabee.meta.js
@@ -2,7 +2,7 @@
 // @id 	 Wasabee.user.js
 // @name 	 IITC plugin: Wasabee Draw Tools
 // @namespace 	 https://wasabee.rocks/
-// @version 	 0.17.20200803134926
+// @version 	 0.17.20200903111452
 // @updateURL 	 https://cdn2.wasabee.rocks/iitcplugin/prod/wasabee.meta.js
 // @downloadURL 	 https://cdn2.wasabee.rocks/iitcplugin/prod/wasabee.user.js
 // @description 	 Wasabee is not for dating.
